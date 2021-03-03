afb3166a841b48ad878bbe8fba1d84426c91f53f
ecophilina <paenglis@lakeheadu.ca>
2021-03-02 19:36:54 -0800

    code for exploring psd outputs; make ggplot version of Viewer() output fig

--- PAM-R/format_dates.R
+++ PAM-R/format_dates.R
@@ -16,3 +16,41 @@ format.dates.spl<-function(file,output="same"){
 }
 
 
+format.psd<-function(file,output="same"){
+  library(tidyverse)
+  d<-read.csv(file, col_names = F)
+
+  freq <- c(t(d[1,2:ncol(d)]))
+  dt <- as.POSIXlt.numeric(as.numeric(d[2:nrow(d),1]$X1), origin = "1970-01-01" ) # extract datetime from first col
+  
+  d2 <- as_tibble(t(d[2:nrow(d), 2:ncol(d)]))
+  colnames(d2) <- dt
+  
+  d2$freq <- freq
+  d2$freq_kHz <- freq/1000
+  
+  d3 <- d2 %>% pivot_longer(cols = 1:45, names_to = "datetime", values_to = "PSD") %>% mutate(
+    DateTime = ymd_hms(datetime),
+    yr=year(DateTime),
+    m=month(DateTime),
+    d=day(DateTime),
+    hr=hour(DateTime),
+    min=minute(DateTime),
+    sec=second(DateTime),
+    date=ymd(paste(yr,m,d)),
+    daily_min=(local_time(DateTime, units = "mins"))
+  ) %>% select(-DateTime)
+  d3$time_elapsed <- as.double(d4$daily_min - min(d4$daily_min))
+  
+  t2<-separate(data.frame(file=file),file,sep=-3,into=c("fn","extra"))
+  
+  if(output=="same"){
+    t2<-separate(data.frame(file=file),file,sep=-3,into=c("fn","extra"))
+    write_rds(d3,paste0(t2[1,1],"rds"))
+  }
+  
+  if(output!="same"){
+    write_rds(d3,paste0(output))
+  }
+  
+}
--- scripts/psd.R
+++ scripts/psd.R
@@ -7,22 +7,36 @@ if(!require(tidyverse))install.packages("tidyverse");library(tidyverse)
 source('PAM-R/PAMGuide-edited.R')
 source('PAM-R/Meta-edited.R')
 source('PAM-R/format_dates.R')
+source('PAM-R/Viewer.R')	
+
+# note: numeric dates are base on origin = "1970-01-01"
+# get the calibration number from the ocean instruments site
+# http://oceaninstruments.azurewebsites.net/App/#/%23
+
+prefix <- "5042"
+calib_value <- -176.2
+set_lcut <- 500
 
 # calling PAMGuide to analyze a single fine
 PAMGuide(
   atype = "PSD",# this tells it you want to do power spectral density 
 #  atype = "Broadband",# this option is for doing SPL
-  outwrite = 1, # this tells it you want to outwrite the analysis file
-  calib = 1, # this tells it you want to use calibration information
-  envi = "Wat",#this tells it you recorded in water
-  ctype = "EE",# this tells it the type of calibration, for soundtraps it is end to end
-  Si = -176.5,# this is where you put the calibration you got from the ocean instruments site
-  lcut = 20,# this is the low frequency cut off
-  hcut = 24000,# this is the high frequency cut off
-  welch = 120,# this tells it you want to average values for 1 second (assuming 50% overlap, which is the default)
+  outwrite = 1,# this tells it you want to outwrite the analysis file
+  calib = 1,# this tells it you want to use calibration information
+  envi = "Wat",# this tells it you recorded in water
+  ctype = "EE",# type of calibration, for soundtraps it is end to end
+  Si = calib_value,# calibration value from the ocean instruments site
+  lcut = set_lcut,# low frequency cut off on Hz
+  hcut = 24000,# high frequency cut off on Hz
+  # welch = 120,# assuming default of 50% overlap, this number is the # seconds x2 (120 for 1 min periods)
+  welch = 40, # for 20 sec periods
   plottype = "none",# tells it whether or not to plot the output
-  timestring = "5678.%y%m%d%H%M%S.wav"# this will allow you to have time stamped data - change the "5678" to match the soundtrap that you are analyzing data from.
+  timestring = paste0(prefix, ".%y%m%d%H%M%S.wav"),# for time stamped data - change the prefix to match the soundtrap used
+  outdir = here::here("data", paste0("lcut-", set_lcut))# output directory within project folder
 )
+
+# this will bring in a dataset and plot it up for you
+Viewer()
 
 # calling PAMMeta to analyze every file in a folder
 PAMMeta(
@@ -32,39 +46,103 @@ PAMMeta(
   calib = 1, # this tells it you want to use calibration information
   envi = "Wat",#this tells it you recorded in water
   ctype = "EE",# this tells it the type of calibration, for soundtraps it is end to end
-  Si = -176.5,# this is where you put the calibration you got from the ocean instruments site
+  Si = calib_value,# this is where you put the calibration you got from the ocean instruments site
   lcut = 20,# this is the low frequency cut off
   hcut = 24000,# this is the high frequency cut off
-  welch = 120,# this tells it you want to average values for 1 second (assuming 50% overlap, which is the default)
+  welch = 120,
   plottype = "none",# tells it whether or not to plot the output
-  timestring = "5678.%y%m%d%H%M%S.wav"# this will allow you to have time stamped data - change the "5678" to match the soundtrap that you are analyzing data from.
+  timestring = paste0(prefix, ".%y%m%d%H%M%S.wav"),
+  outdir = here::here("data")
 )
 
 # this will bring in a dataset and plot it up for you
 Viewer()
 
-# the code below brings in SPL files, and formats the date and saves them again. 
-#this should be fairly self explanatory but let me know if you have trouble with it.
-# now format dates
-fdr<-list.dirs("D:\\OA5\\5678")
-ftd<-list.files(fdr[3],pattern = "*.csv")
 
-#move all the individual wave files into the oyster acoustic folder
-for(i in 1:length(ftd)){
-  f1<-paste0(fdr[3],"\\",ftd[i])
-  t1<-separate(data.frame(nms=ftd[i]),nms,sep=17,into=c("wavfile","extra"))
-  format.dates.spl(f1,
-                   output=paste0("C:\\Users\\sarcher\\Documents\\Manuscripts\\oyster_acoustics\\02_odata\\spl_raw\\OA5\\",
-                                 t1[1,1],".rds"))
+# exploring raw data output from PAMGuide
+library(readr)
+library(lubridate)
+library(tidyverse)
+
+fullfile <- file.choose()		
+d <- read_csv(fullfile, col_names = F)
+
+# the code below produces figure identical to Viewer() for this file on PE's system
+# d <- read_csv("data/lcut-500/5042.200308223002_PSD_96000samplesHannWindow_50PercentOverlap.csv", col_names = F)
+
+freq <- c(t(d[1,2:ncol(d)]))
+dt <- as.POSIXlt.numeric(as.numeric(d[2:nrow(d),1]$X1), origin = "1970-01-01" ) # extract datetime from first col
+
+d2 <- as_tibble(t(d[2:nrow(d), 2:ncol(d)]))
+colnames(d2) <- dt
+
+d2$freq <- freq
+d2$freq_kHz <- freq/1000
+
+d3 <- d2 %>% pivot_longer(cols = 1:45, names_to = "datetime", values_to = "PSD") %>% mutate(
+  DateTime = ymd_hms(datetime),
+  yr=year(DateTime),
+  m=month(DateTime),
+  d=day(DateTime),
+  hr=hour(DateTime),
+  min=minute(DateTime),
+  sec=second(DateTime),
+  date=ymd(paste(yr,m,d)),
+  t=as.POSIXct(datetime, origin = "1970-01-01"),
+  daily_min=(local_time(DateTime, units = "mins"))
+) %>% select(-DateTime)
+d3$time_elapsed <- as.double(d4$daily_min - min(d4$daily_min))
+
+tdiff <- max(d3$t) - min(d3$t) # define time format for x-axis of time plot depending on scale
+
+if (tdiff < 10) {
+  tform <- "%H:%M:%S:%OS3"
+} else if (tdiff > 10 & tdiff < 86400) {
+  tform <- "%H:%M:%S"
+} else if (tdiff > 86400 & tdiff < 86400 * 7) {
+  tform <- "%H:%M \n %d %b"
+} else if (tdiff > 86400 * 7) {
+  tform <- "%d %b %y"
 }
 
-#move the concatenated full-deployment dataset to the odata folder of oyster acoustics
-ftd<-list.files(fdr[1],pattern = "*.csv")
 
-for(i in 1:length(ftd)){
-  f1<-paste0(fdr[1],"\\",ftd[i])
-  t1<-separate(data.frame(nms=ftd[i]),nms,sep=17,into=c("wavfile","extra"))
-  format.dates.spl(f1,output="C:\\Users\\sarcher\\Documents\\Manuscripts\\oyster_acoustics\\02_odata\\OA5_5678.rds")
-}
+d3$time <- as.POSIXct(d3$t,format = tform)
 
-Viewer()
+jet.colors <- colorRampPalette(c("#00007F", "blue", "#007FFF", "cyan", "#7FFF7F", "yellow", "#FF7F00", "red", "#7F0000"))
+ggplot(d3, aes(time, freq_kHz, colour = psd)) + geom_tile() + 
+  # scale_colour_viridis_c() + 
+  scale_y_log10() +
+  scale_colour_gradientn(colours = jet.colors(512)) +
+  ylab("Frequency (kHz)") +
+  xlab("Time") +
+  coord_cartesian(expand = F) +
+  annotation_logticks(sides = "l") +
+  ggsidekick::theme_sleek() 
+
+
+# # the code below brings in SPL files, and formats the date and saves them again. 
+# # not yet applied to herring data or modified to format PSD files using format.psd()
+# 
+# fdr<-list.dirs("D:\\OA5\\5678")
+# ftd<-list.files(fdr[3],pattern = "*.csv")
+# 
+# # move all the individual wave files into the oyster acoustic folder
+# for(i in 1:length(ftd)){
+#   f1<-paste0(fdr[3],"\\",ftd[i])
+#   t1<-separate(data.frame(nms=ftd[i]),nms,sep=17,into=c("wavfile","extra"))
+#   format.dates.spl(f1,
+#     output=paste0(
+#       "C:\\Users\\sarcher\\Documents\\Manuscripts\\oyster_acoustics\\02_odata\\spl_raw\\OA5\\",
+#       t1[1,1],".rds"))
+# }
+# 
+# # move the concatenated full-deployment dataset to the odata folder of oyster acoustics
+# ftd<-list.files(fdr[1],pattern = "*.csv")
+# 
+# for(i in 1:length(ftd)){
+#   f1<-paste0(fdr[1],"\\",ftd[i])
+#   t1<-separate(data.frame(nms=ftd[i]),nms,sep=17,into=c("wavfile","extra"))
+#   format.dates.spl(f1,
+#     output="C:\\Users\\sarcher\\Documents\\Manuscripts\\oyster_acoustics\\02_odata\\OA5_5678.rds")
+# }
+
