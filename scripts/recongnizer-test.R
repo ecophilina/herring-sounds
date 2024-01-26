a588a3d11a25a6a597fc09541203e0bd34cd9b7b
ecophilina <paenglis@lakeheadu.ca>
2024-01-25 16:58:18 -0800

    draft script for checking recognizer performance against all annotations

---
+++ scripts/recongnizer-test.R
@@ -0,0 +1,148 @@
+# script to check recognizer performance against all annotations
+library(tidyverse)
+library(patchwork)
+
+## doesn't work with data from python model
+# library(Rraven)
+# mdat <-imp_raven(path="model-annotations/collishaw_annotations/",
+#                  # only.spectro.view = FALSE,
+#                  # recursive = TRUE,
+#                  # name.from.file = TRUE,
+#                  # ext.case = "lower",
+#                  all.data = TRUE
+#                  )
+
+read_plus <- function(flnm) {
+  data.table::fread(flnm) %>%
+    mutate(filename = flnm)
+}
+
+mdat1 <-
+  list.files(paste0("model-annotations/collishaw_annotations"), pattern = ".txt", full.names = TRUE) |>
+  map_df(~read_plus(.))
+mdat2 <-
+  list.files(paste0("model-annotations/denman_annotations"), pattern = ".txt", full.names = TRUE) |>
+  map_df(~read_plus(.))
+mdat3 <-
+  list.files(paste0("model-annotations/neckpt_annotations"), pattern = ".txt", full.names = TRUE) |>
+  map_df(~read_plus(.))
+
+
+mdat <- bind_rows(mdat1, mdat2, mdat3) %>%
+  separate(filename, sep=c("\\."), 
+           into=c("soundtrap","datetime","datatype","filetype"),
+           remove = F, extra = "merge") %>%
+  separate(datetime,
+           into = c("year","month","day","hr","min","sec"),
+           sep=c(-10,-8,-6,-4,-2),
+           remove=FALSE) %>%
+  separate(soundtrap, sep=c("\\/"), 
+           into = c("datatype2","sitefolder","soundtrap"),
+           remove=FALSE)  %>%
+  separate(sitefolder, sep=c("\\_"), 
+           into = c("site","scratch"),
+           remove=FALSE) %>%
+  mutate(site = case_when(site == "collishaw" ~ "Collishaw (2020)",
+                          site == "denman" ~ "Denman (2020)", 
+                          site == "neckpt" ~ "Neck Point (2021)")) %>% 
+  mutate(duration = `End Time (s)` - `Begin Time (s)`)
+
+
+# load summary of annotations at file level created by annotations-plots.R
+alldat <- readRDS("wdata/all-annotations3.rds") 
+
+filedat2 <- alldat %>% filter(site !="Neck Point (2020)", filename !="") %>% 
+  group_by(filename, datetime, site) %>%
+  mutate(n = n()) %>%
+  summarise(
+    `Herring` = ifelse(herring.hs > 0 & herring.hs <= 1, 1, round(mean(herring.hs))),
+    `Pinnipeds` = ifelse(pinniped > 0 & pinniped <= 1, 1, round(mean(pinniped))),
+    `Birds` = ifelse(gull > 0 & gull  <= 1, 1, round(mean(gull ))),
+    `Boat noise` = ifelse(boat>0 & boat <= 1, 1, round(mean(boat))),
+    `Wave noise` = ifelse(waves>0 & waves <= 1, 1, round(mean(waves))),
+    `Fish knocks` = ifelse(fish.knock>0 & fish.knock <= 1, 1, round(mean(fish.knock, na.rm = T))),
+    `Invertebrate snaps` = ifelse(invert.snap>0 & invert.snap <= 1, 1, round(mean(invert.snap, na.rm = T))),
+    `Splashing` = ifelse(splahes>0 & splahes <= 1, 1, round(mean(splahes, na.rm = T))),
+    `Other mechanical` = ifelse(rustling>0 & rustling <= 1, 1, round(mean(rustling, na.rm = T))),
+    `Pinniped deterrent` = ifelse(tonal>0 & tonal <= 1, 1, round(mean(tonal, na.rm = T))),
+    n = mean(n)
+  ) %>% pivot_longer(4:13, names_to = "sound", values_to = "score") %>%
+  mutate(
+    # sound = factor(sound, levels = c(
+    # `Herring FRT`, `Rustling`, `Splashing`, 
+    # `Sea lions`, `Gulls`, `Sea lion deterrent`,
+    # `Fish knocks`, `Invertebrate snaps`, 
+    # `Boat noise`, `Wave noise`
+    # )),
+    Site = as.factor(site)
+  )
+
+filedat2$sound <- factor(filedat2$sound, levels = c("Herring", 
+                                                    "Boat noise", "Wave noise", 
+                                                    "Other mechanical", 
+                                                    "Birds", "Pinnipeds", "Splashing",
+                                                    "Fish knocks", "Invertebrate snaps", 
+                                                    "Pinniped deterrent"
+))
+
+filedat2$Site <- factor(filedat2$site, 
+                        levels = c("Collishaw (2020)", "Denman (2020)", "Neck Point (2021)"))
+
+
+# mdatsum <- 
+0.97
+
+threshold_boxplot <- function(set_threshold, sound_type = "Herring") {
+mdat %>% filter(Score > set_threshold) %>% 
+  group_by(datetime, site) %>% 
+  reframe(detection_time = sum(duration)) %>% left_join(filedat2, .) %>% 
+    filter(sound == sound_type) %>%
+  ggplot() + 
+  geom_boxplot(aes(as.factor(score), detection_time)) + 
+  facet_wrap(~site) + 
+  xlab(paste("Manually annotated", sound_type, "Score")) +
+  ylab("Total duration of detections (s)") +
+  ggtitle(paste("Detection score >", set_threshold)) +  
+  ggsidekick::theme_sleek()
+}
+
+library(patchwork)
+
+boxplot_series <- function(
+    threshold_series = c(0.75, 0.85, 0.95, 0.99), 
+    sound_type = "Herring") {
+p1 <- threshold_boxplots(threshold_series[1], sound_type) + theme(axis.title = element_blank())
+p2 <- threshold_boxplots(threshold_series[2], sound_type)+ theme(
+  strip.text = element_text(size=0),
+  axis.title = element_blank())
+p3 <- threshold_boxplots(threshold_series[3], sound_type)+ theme(
+  strip.text = element_text(size=0),
+  axis.title = element_blank())
+p4 <- threshold_boxplots(threshold_series[4], sound_type) + theme(
+  strip.text = element_text(size=0),
+  axis.title.y = element_blank())
+
+y_lab_big <- ggplot() +
+  annotate(geom = "text", x = 1, y = 1, size = 4,
+           label = "Total duration of detections (s)", angle = 90) +
+  coord_cartesian(clip = "off")+
+  theme_void()
+
+y_lab_big + wrap_plots(p1, p2 , p3 , p4 , ncol = 1) +
+  patchwork::plot_layout(widths = c(0.05,1))
+}
+
+boxplot_series()
+ggsave("figs/detector-threshold-boxplots.png")
+
+
+boxplot_series(sound_type = "Wave noise") 
+ggsave("figs/detector-threshold-boxplots-waves.png")
+
+
+boxplot_series(sound_type = "Boat noise") 
+ggsave("figs/detector-threshold-boxplots-boats.png")
+
+
+boxplot_series(sound_type = "Other mechanical") 
+ggsave("figs/detector-threshold-boxplots-other.png")
